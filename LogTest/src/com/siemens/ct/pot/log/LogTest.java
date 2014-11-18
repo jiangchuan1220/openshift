a25524e96231388eae1984dab11ddbc19253907d
jiangchuan1220@163.com <jiangchuan1220@163.com>
2014-11-18 15:12:26 +0800

    log_test
    log_test

---
+++ LogTest/src/com/siemens/ct/pot/log/LogTest.java
@@ -0,0 +1,76 @@
+package com.siemens.ct.pot.log;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class LogTest extends HttpServlet {
+	public static Logger logger = Logger.getLogger(LogTest.class);
+	/**
+	 * Constructor of the object.
+	 */
+	public LogTest() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		File file = new File(this.getServletContext().getRealPath("/") + "/log4j.properties");
+		System.out.println(file.getAbsolutePath());
+		PropertyConfigurator.configure(file.getAbsolutePath());
+		logger.debug("This is a debug message");
+		logger.info("This is an info message");
+		logger.error("This is an error message");
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
